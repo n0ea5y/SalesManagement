9b1f2d4ddc4436624633fb5b6c85c33d927ad705
n0ea5y <n0ea5y2024@gmail.com>
2025-06-01 18:15:17 +0900

    エクセルのライブラリ追加 テンプレートエクセル追加

---
+++ app/src/views/Tools/constants.js
@@ -0,0 +1,13 @@
+export const SHEET_NAME = ['売上合計', '原価', '外板集計', '人件費'];
+export const MEDIA_FIELDS = ['組数', '人数', '合計金額'];
+
+
+export const MEDIA_LIST = [
+  {name: 'Ratty', row: 'C'},
+  {name: 'hotpepper', row: 'F'},
+  {name: '食べログ', row: 'I'},
+  {name: 'ぐるなび', row: 'U'},
+  {name: 'フリー', row: 'X'},
+  {name: 'SNS', row: 'AA'},
+  {name: '外板', row: 'AD'},
+]
